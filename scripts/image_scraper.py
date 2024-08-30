6c371e5c4c36e2c231c3e3e21889c154c49e7ed4
Chris Raiz <cwraiz@icloud.com>
2024-08-30 16:55:32 -0500

    Update image_scraper.py
    Import time library
    Import WebDriverException
    
    Update path for id's csv
    Create path for image csv
    Create path for timeout csv
    Create path for invalid csv
    
    Update range to accommodate all rows in the IDs csv
    
    Implement try/except blocks for the driver.get() function, except catches WebDriverExceptions
    
    Update path parameter for creating the csv files

--- scripts/image_scraper.py
+++ scripts/image_scraper.py
@@ -1,29 +1,33 @@
 # Imports
 import csv
 import pandas
+import time
 from pathlib import Path
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-from selenium.common.exceptions import TimeoutException
+from selenium.common.exceptions import TimeoutException, WebDriverException
+
+csv_path = './../BC-WoW/ID/BC_IDs.csv'
 
 # Path to valid_ids.csv
-file = Path(__file__).parent / "valid_ids.csv"
-
+id_path = Path(__file__).parent / csv_path
+images_path = Path(__file__).parent / './../BC-WoW/Data/BC_NPC_Images.csv'
+timeout_path = Path(__file__).parent / './../BC-WoW/Timeout/BC_Images_Timeout_IDs.csv'
+invalid_path = Path(__file__).parent / './../BC-WoW/Invalid/BC_Images_Invalid_IDs.csv'
 # Read valid_ids.csv (Pandas)
-df = pandas.read_csv(file)
+df = pandas.read_csv(id_path)
+end = len(df)
+
 
 # Timeout IDs
-timeout_id_data = "Timeout_IDs.csv"
 timeout_ids = []
 
 # Invalid IDs
-invalid_id_data = "Invalid_IDs.csv"
 invalid_ids = []
 
 # NPC Data
-npc_data = "NPC_Images.csv"
 npc_image_data = []
 
 # Webdriver
@@ -32,35 +36,43 @@ options.add_argument("--headless=new")
 driver = webdriver.Chrome(options=options)
 
 # Gather img data for npc id (start, end)
-for row in range(0, 20725):
+for row in range(0, end):
   npc_id = df.get("ID")[row] # Get ID from valid_ids.csv
-  driver.get(f"https://www.wowhead.com/npc={npc_id}")
-
   print(npc_id)
 
-  # Gather all img tags on page
   try:
-    imgs = WebDriverWait(driver, 10).until(EC.presence_of_all_elements_located((By.TAG_NAME, 'img')))
-  except TimeoutException:
-    timeout_ids.append({'ID': npc_id})
-    continue
+    driver.get(f"https://www.wowhead.com/npc={npc_id}")
+    time.sleep(2)
 
-  # Find the npc img
-  for img in imgs:
-    img_src = img.get_attribute('src')
-    img_class = img.get_attribute('class')
-    if img_class == 'border' and 'https://wow.zamimg.com/' in img_src:
-      npc_image_data.append({'ID': npc_id, 'Image': img_src})
+    image_found = False
+
+    # Gather all img tags on page
+    try:
+      imgs = WebDriverWait(driver, 20).until(EC.presence_of_all_elements_located((By.TAG_NAME, 'img')))
+      for img in imgs:
+        img_src = img.get_attribute('src')
+        img_class = img.get_attribute('class')
+        if img_class == 'border' and 'https://wow.zamimg.com/' in img_src:
+          image_found = True
+          npc_image_data.append({'ID': npc_id, 'Image': img_src})
+          break
+      if image_found:
+        continue
+    except TimeoutException:
+      timeout_ids.append({'ID': npc_id})
       continue
 
-  # Invalid ID if loop is not continued
-  invalid_ids.append({'ID': npc_id})
+    # Invalid ID if loop is not continued
+    invalid_ids.append({'ID': npc_id})
+
+  except WebDriverException:
+    timeout_ids.append({'ID': npc_id})
 
 # Release the resources allocated by Selenium and shut down the browser
 driver.quit()
 
 # Npc image data to .csv file
-with open(npc_data, mode="w", newline="", encoding="utf-8") as file:
+with open(images_path, mode="w", newline="", encoding="utf-8") as file:
     #? Write the headers
     writer = csv.DictWriter(file, fieldnames=["ID", "Image"])
     writer.writeheader()
@@ -68,13 +80,13 @@ with open(npc_data, mode="w", newline="", encoding="utf-8") as file:
     writer.writerows(npc_image_data)
 
 # Invalid ID data to .csv file
-with open(invalid_id_data, mode='w', newline="", encoding='utf-8') as file:
+with open(invalid_path, mode='w', newline="", encoding='utf-8') as file:
   writer = csv.DictWriter(file, fieldnames=["ID"])
   writer.writeheader()
   writer.writerows(invalid_ids)
 
 # Timeout ID data to .csv file
-with open(timeout_id_data, mode='w', newline="", encoding='utf-8') as file:
+with open(timeout_path, mode='w', newline="", encoding='utf-8') as file:
   writer = csv.DictWriter(file, fieldnames=["ID"])
   writer.writeheader()
   writer.writerows(timeout_ids)
